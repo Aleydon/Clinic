1e1bad0fd930dfe50e42017a90e1dbd86863604e
Aleydon <aleydonroberto@gmail.com>
2025-05-26 20:20:12 -0300

    feat: added clinic, doctor, patient and their relationships tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
 import {
   integer,
+  pgEnum,
   pgTable,
   text,
   time,
@@ -30,6 +31,39 @@ export const doctorsTable = pgTable('doctors', {
   availableFromTime: time('available_from_time').notNull(),
   availableToTime: time('available_to_time').notNull(),
   specialty: text('specialty').notNull(),
+  appointmentPriceInCents: integer('appointment_price_in_cents').notNull(),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .defaultNow()
+    .$onUpdate(() => new Date())
+});
+
+export const patientSexEnum = pgEnum('patient_sex', ['male', 'female']);
+
+export const PatientsTable = pgTable('patients', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  name: text('name').notNull(),
+  email: text('email').notNull(),
+  phoneNumber: text('phone_number').notNull(),
+  sex: patientSexEnum('sex').notNull(),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .defaultNow()
+    .$onUpdate(() => new Date())
+});
+
+export const appointmentsTable = pgTable('appointments', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  date: timestamp('date').notNull(),
+  clinicId: uuid('clinic_id')
+    .notNull()
+    .references(() => clinicsTable.id, { onDelete: 'cascade' }),
+  patientId: uuid('patient_id')
+    .notNull()
+    .references(() => PatientsTable.id, { onDelete: 'cascade' }),
+  doctorId: uuid('doctor_id')
+    .notNull()
+    .references(() => doctorsTable.id, { onDelete: 'cascade' }),
   createdAt: timestamp('created_at').defaultNow(),
   updatedAt: timestamp('updated_at')
     .defaultNow()
