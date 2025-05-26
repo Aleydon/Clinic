5f43418ee158c02eea0cbab5fe704419653bb3c2
Aleydon <aleydonroberto@gmail.com>
2025-05-26 13:41:26 -0300

    chore: Initial commit project

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h4 className="text-3xl font-bold">Clinic (SaaS)</h4>
+    </div>
+  );
+}
