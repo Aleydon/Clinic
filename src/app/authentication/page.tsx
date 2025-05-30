706a79557409e87ad824c104c888d5dd02dcbb72
Aleydon <aleydonroberto@gmail.com>
2025-05-30 11:48:33 -0300

    chore: add tabs from shadcn-ui

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,7 @@
+export default function AuthenticationPage() {
+  return (
+    <div className="flex items-center justify-center h-screen w-screen bg-amber-800">
+      <h2 className="text-4xl text-bold">AuthenticationPage</h2>
+    </div>
+  );
+}
