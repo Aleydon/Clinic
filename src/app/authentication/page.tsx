721678dbdb4fc07a10ddb8f4247505d6861b0135
Aleydon <aleydonroberto@gmail.com>
2025-05-30 12:07:52 -0300

    chore: add card, input, label e tabs from shadcn-ui

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,7 +1,53 @@
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card';
+// import { Input } from '@/components/ui/input';
+// import { Label } from '@/components/ui/label';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
 export default function AuthenticationPage() {
   return (
-    <div className="flex items-center justify-center h-screen w-screen bg-amber-800">
-      <h2 className="text-4xl text-bold">AuthenticationPage</h2>
+    <div className="flex items-center justify-center h-screen w-screen">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Tabs defaultValue="login">
+          <TabsList>
+            <TabsTrigger value="login">Login</TabsTrigger>
+            <TabsTrigger value="register">Register</TabsTrigger>
+          </TabsList>
+          <TabsContent value="login">
+            <Card>
+              <CardHeader>
+                <CardTitle>Login</CardTitle>
+                <CardDescription>Log in to continue</CardDescription>
+              </CardHeader>
+              <CardContent className="grid gap-6"></CardContent>
+              <CardFooter>
+                <Button>Login</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="register">
+            <Card>
+              <CardHeader>
+                <CardTitle>Register</CardTitle>
+                <CardDescription>
+                  Create a new account to get started
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="grid gap-6"></CardContent>
+              <CardFooter>
+                <Button>Create account</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 }
