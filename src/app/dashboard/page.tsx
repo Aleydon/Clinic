2a51958cc6a3e760f138dc1d812894f36661c893
Aleydon <aleydonroberto@gmail.com>
2025-06-01 18:49:35 -0300

    feat: get user session info and add logout function

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,25 @@
-export default function Dashboard() {
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+import { auth } from '@/lib/auth';
+
+import { SignOut } from './components/sign-out';
+
+export default async function Dashboard() {
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (!session) {
+    redirect('/authentication');
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h3 className="text-4xl text-bold">Dashboard</h3>
+      <h3 className="text-4xl text-bold">
+        {session?.user?.name
+          ? `Welcome back, ${session.user.name}!`
+          : 'Welcome to the Dashboard!'}
+      </h3>
+      <SignOut />
     </div>
   );
 }
